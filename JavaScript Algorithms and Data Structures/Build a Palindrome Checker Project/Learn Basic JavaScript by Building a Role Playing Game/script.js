877c43b249d305dbc5029044e60570bdcebb7cff
Uzy777 <usmon.h@outlook.com>
2025-02-07 20:27:12 +0000

    Add In Progress Building a Role Playing Game

--- JavaScript Algorithms and Data Structures/Build a Palindrome Checker Project/Learn Basic JavaScript by Building a Role Playing Game/script.js
+++ JavaScript Algorithms and Data Structures/Build a Palindrome Checker Project/Learn Basic JavaScript by Building a Role Playing Game/script.js
@@ -16,6 +16,7 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+
 const locations = [
   {
     name: "town square",
@@ -28,6 +29,12 @@ const locations = [
     "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
     "button functions": [buyHealth, buyWeapon, goTown],
     text: "You enter the store.",
+  },
+  {
+    name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button functions": [fightSlime, fightBeast, goTown],
+    text: "You enter the cave. You see some monsters.",
   },
 ];
 
@@ -43,23 +50,38 @@ function update(location) {
   button1.onclick = location["button functions"][0];
   button2.onclick = location["button functions"][1];
   button3.onclick = location["button functions"][2];
-  text.innerText = 'You are in the town square. You see a sign that says "Store".';
+  text.innerText = location.text;
 }
 
 function goTown() {
   update(locations[0]);
 }
 
-function goStore() {}
+function goStore() {
+  update(locations[1]);
+}
 
 function goCave() {
-  console.log("Going to cave.");
+  update(locations[2]);
 }
 
 function fightDragon() {
   console.log("Fighting dragon.");
 }
 
-function buyHealth() {}
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
+  }
+}
 
 function buyWeapon() {}
+
+function fightSlime() {}
+
+function fightBeast() {}
