1dfa964fddf62c6b0f7a5475bf2f8ebca991eb89
Uzy777 <usmon.h@outlook.com>
2025-02-07 21:18:33 +0000

    Add In Progress Building a Role Playing Game

--- JavaScript Algorithms and Data Structures/Build a Palindrome Checker Project/Learn Basic JavaScript by Building a Role Playing Game/script.js
+++ JavaScript Algorithms and Data Structures/Build a Palindrome Checker Project/Learn Basic JavaScript by Building a Role Playing Game/script.js
@@ -16,7 +16,12 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
-
+const weapons = [
+  { name: "stick", power: 5 },
+  { name: "dagger", power: 30 },
+  { name: "claw hammer", power: 50 },
+  { name: "sword", power: 100 },
+];
 const locations = [
   {
     name: "town square",
@@ -80,7 +85,11 @@ function buyHealth() {
   }
 }
 
-function buyWeapon() {}
+function buyWeapon() {
+  if (gold >= 30) {
+    gold -= 30;
+  }
+}
 
 function fightSlime() {}
 
