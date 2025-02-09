7550607b31666dbc14e480735cb695ff641e23df
Uzy777 <usmon.h@outlook.com>
2025-02-09 15:54:11 +0000

    Add In Progress Building a Role Playing Game

--- JavaScript Algorithms and Data Structures/Build a Palindrome Checker Project/Learn Basic JavaScript by Building a Role Playing Game/script.js
+++ JavaScript Algorithms and Data Structures/Build a Palindrome Checker Project/Learn Basic JavaScript by Building a Role Playing Game/script.js
@@ -22,6 +22,7 @@ const weapons = [
   { name: "claw hammer", power: 50 },
   { name: "sword", power: 100 },
 ];
+
 const locations = [
   {
     name: "town square",
@@ -70,10 +71,6 @@ function goCave() {
   update(locations[2]);
 }
 
-function fightDragon() {
-  console.log("Fighting dragon.");
-}
-
 function buyHealth() {
   if (gold >= 10) {
     gold -= 10;
@@ -107,9 +104,20 @@ function buyWeapon() {
 
 function sellWeapon() {
   if (inventory.length > 1) {
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "Don't sell your only weapon!";
   }
 }
 
 function fightSlime() {}
 
 function fightBeast() {}
+
+function fightDragon() {
+  console.log("Fighting dragon.");
+}
