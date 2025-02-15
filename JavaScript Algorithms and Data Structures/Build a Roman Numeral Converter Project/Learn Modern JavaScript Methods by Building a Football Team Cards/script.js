e522f5db2097688e0c487b13f4ad8466f6efcc80
Uzy777 <usmon.h@outlook.com>
2025-02-15 13:49:48 +0000

    Update In Progress Building Football Team Cards

--- JavaScript Algorithms and Data Structures/Build a Roman Numeral Converter Project/Learn Modern JavaScript Methods by Building a Football Team Cards/script.js
+++ JavaScript Algorithms and Data Structures/Build a Roman Numeral Converter Project/Learn Modern JavaScript Methods by Building a Football Team Cards/script.js
@@ -180,4 +180,12 @@ teamName.textContent = team;
 worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
-const setPlayerCards = (arr = players) => {};
+const setPlayerCards = (arr = players) => {
+  playerCards.innerHTML += arr.map(({ name, position, number, isCaptain, nickname }) => {
+    return `
+        <div class="player-card">
+          <h2>${name}</h2>
+        </div>
+      `;
+  });
+};
