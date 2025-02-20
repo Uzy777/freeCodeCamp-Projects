7ff64b7c851def9ab691c4990e7a4a18863c3d05
Uzy777 <usmon.h@outlook.com>
2025-02-20 22:24:24 +0000

    Update In Progress Building a Decimal to Binary Converter

--- JavaScript Algorithms and Data Structures/Build a Roman Numeral Converter Project/Learn Recursion by Building a Decimal to Binary Converter/script.js
+++ JavaScript Algorithms and Data Structures/Build a Roman Numeral Converter Project/Learn Recursion by Building a Decimal to Binary Converter/script.js
@@ -3,19 +3,14 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  let binary = "";
-
-  if (input === 0) {
-    binary = "0";
+  if (input === 0 || input === 1) {
+    return String(input);
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
-
-  while (input > 0) {
-    binary = (input % 2) + binary;
-    input = Math.floor(input / 2);
-  }
-
-  result.innerText = binary;
 };
+
+const showAnimation = () => {};
 
 const checkUserInput = () => {
   if (!numberInput.value || isNaN(parseInt(numberInput.value)) || parseInt(numberInput.value) < 0) {
@@ -23,7 +18,12 @@ const checkUserInput = () => {
     return;
   }
 
-  decimalToBinary(parseInt(numberInput.value));
+  if (parseInt(numberInput.value) === 5) {
+    showAnimation();
+    return;
+  }
+
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
   numberInput.value = "";
 };
 
