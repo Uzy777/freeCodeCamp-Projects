678d5743b4e57aa6287eb85cad9dc8a781a572d3
Uzy777 <usmon.h@outlook.com>
2025-03-16 21:53:31 +0000

    Update In Progress Building a Number Sorter

--- JavaScript Algorithms and Data Structures/Build a Telephone Number Validator Project/Learn Basic Algorithmic Thinking by Building a Number Sorter/script.js
+++ JavaScript Algorithms and Data Structures/Build a Telephone Number Validator Project/Learn Basic Algorithmic Thinking by Building a Number Sorter/script.js
@@ -4,6 +4,21 @@ const sortInputArray = (event) => {
   event.preventDefault();
 
   const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
+
+  updateUI(inputValues);
+};
+
+const updateUI = (array = []) => {
+  array.forEach((num, i) => {
+    const outputValueNode = document.getElementById(`output-value-${i}`);
+    outputValueNode.innerText = num;
+  });
+};
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {}
+  }
 };
 
 sortButton.addEventListener("click", sortInputArray);
