3492dd3181cfc3e92dc96d55efb2f83bf4ba46b3
Uzy777 <usmon.h@outlook.com>
2025-03-24 21:43:51 +0000

    Update In Progress Building a Statistics Calculator

--- JavaScript Algorithms and Data Structures/Build a Telephone Number Validator Project/Learn Advanced Array Methods by Building a Statistics Calculator/script.js
+++ JavaScript Algorithms and Data Structures/Build a Telephone Number Validator Project/Learn Advanced Array Methods by Building a Statistics Calculator/script.js
@@ -25,6 +25,9 @@ const getRange = (array) => {
 
 const getVariance = (array) => {
   const mean = getMean(array);
+  const differences = array.map((el) => el - mean);
+  const squaredDifferences = differences.map((el) => el ** 2);
+  const sumSquaredDifferences = squaredDifferences.reduce((acc, el) => acc + el, 0);
 };
 
 const calculate = () => {
