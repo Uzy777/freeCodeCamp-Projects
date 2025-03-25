0a289c827d86fb4d289dfd74744f0fe66d57d651
Uzy777 <usmon.h@outlook.com>
2025-03-25 18:39:25 +0000

    Update In Progress Building a Spreadsheet

--- JavaScript Algorithms and Data Structures/Build a Telephone Number Validator Project/Learn Functional Programming by Building a Spreadsheet/script.js
+++ JavaScript Algorithms and Data Structures/Build a Telephone Number Validator Project/Learn Functional Programming by Building a Spreadsheet/script.js
@@ -13,4 +13,9 @@ window.onload = () => {
     container.appendChild(label);
   };
   const letters = charRange("A", "J");
+  letters.forEach(createLabel);
+  range(1, 99).forEach((number) => {
+    createLabel(number);
+    letters.forEach((letter) => {});
+  });
 };
