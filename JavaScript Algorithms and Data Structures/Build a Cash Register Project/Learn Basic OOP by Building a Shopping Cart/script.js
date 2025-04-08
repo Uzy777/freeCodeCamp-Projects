2cebf6675311daadc520749ad7ab93158ac4d6ee
Uzy777 <usmon.h@outlook.com>
2025-04-08 20:54:47 +0100

    Update In Progress Building a Shopping Cart

--- JavaScript Algorithms and Data Structures/Build a Cash Register Project/Learn Basic OOP by Building a Shopping Cart/script.js
+++ JavaScript Algorithms and Data Structures/Build a Cash Register Project/Learn Basic OOP by Building a Shopping Cart/script.js
@@ -85,4 +85,16 @@ const products = [
   },
 ];
 
-products.forEach(() => {});
+products.forEach(({ name, id, price, category }) => {
+  dessertCards.innerHTML += `
+      <div class="dessert-card">
+        <h2>${name}</h2>
+        <p class="dessert-price">$${price}</p>
+        <p class="product-category">Category: ${category}</p>
+        <button 
+          id="${id}" 
+          class="btn add-to-cart-btn">Add to cart
+        </button>
+      </div>
+    `;
+});
