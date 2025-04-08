2b813da854775ea8bff30d4ca14cb8257b460460
Uzy777 <usmon.h@outlook.com>
2025-04-08 22:26:50 +0100

    Update In Progress Building a Shopping Cart

--- JavaScript Algorithms and Data Structures/Build a Cash Register Project/Learn Basic OOP by Building a Shopping Cart/script.js
+++ JavaScript Algorithms and Data Structures/Build a Cash Register Project/Learn Basic OOP by Building a Shopping Cart/script.js
@@ -131,3 +131,17 @@ class ShoppingCart {
       `);
   }
 }
+
+const cart = new ShoppingCart();
+const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
+
+[...addToCartBtns].forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    cart.addItem(Number(event.target.id), products);
+  });
+});
+
+cartBtn.addEventListener("click", () => {
+  isCartShowing = !isCartShowing;
+  showHideCartSpan.textContent = isCartShowing ? "Hide" : "Show";
+});
