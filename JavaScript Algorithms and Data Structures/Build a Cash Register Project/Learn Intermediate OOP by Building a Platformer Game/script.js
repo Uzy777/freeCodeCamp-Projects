0e84a537cb13be3953505634440a4f35415c02cc
Uzy777 <usmon.h@outlook.com>
2025-04-11 16:50:19 +0100

    Update In Progress Building a Platformer Game

--- JavaScript Algorithms and Data Structures/Build a Cash Register Project/Learn Intermediate OOP by Building a Platformer Game/script.js
+++ JavaScript Algorithms and Data Structures/Build a Cash Register Project/Learn Intermediate OOP by Building a Platformer Game/script.js
@@ -15,6 +15,21 @@ const proportionalSize = (size) => {
 
 class Player {
   constructor() {
-    this.position = {};
+    this.position = {
+      x: proportionalSize(10),
+      y: proportionalSize(400),
+    };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    this.width = proportionalSize(40);
+    this.height = proportionalSize(40);
   }
+  draw() {
+    ctx.fillStyle = "#99c9ff";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {}
 }
