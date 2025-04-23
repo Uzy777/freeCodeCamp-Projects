bfc653b36ef6eec7bcacebf2938e7e63353ae135
Uzy777 <usmon.h@outlook.com>
2025-04-23 21:47:39 +0100

    Update In Progress Building a Dice Game

--- JavaScript Algorithms and Data Structures/Build a Cash Register Project/Review Algorithmic Thinking by Building a Dice Game/script.js
+++ JavaScript Algorithms and Data Structures/Build a Cash Register Project/Review Algorithmic Thinking by Building a Dice Game/script.js
@@ -117,3 +117,31 @@ rulesBtn.addEventListener("click", () => {
     rulesContainer.style.display = "none";
   }
 });
+
+keepScoreBtn.addEventListener("click", () => {
+  let selectedValue;
+  let achieved;
+
+  for (const radioButton of scoreInputs) {
+    if (radioButton.checked) {
+      selectedValue = radioButton.value;
+      achieved = radioButton.id;
+      break;
+    }
+  }
+
+  if (selectedValue) {
+    rolls = 0;
+    round++;
+    updateStats();
+    resetRadioOptions();
+    updateScore(selectedValue, achieved);
+    if (round > 6) {
+      setTimeout(() => {
+        alert(`Game Over! Your total score is ${score}`);
+      }, 500);
+    }
+  } else {
+    alert("Please select an option or roll the dice");
+  }
+});
