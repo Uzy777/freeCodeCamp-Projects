64cb53f6daa44fbc5dcfe009bb6bc41fa7e5e76e
Uzy <132500083+Uzy777@users.noreply.github.com>
2025-04-24 11:25:06 +0100

    Update In Progress Final Project Build a Cash Register

--- JavaScript Algorithms and Data Structures/Build a Cash Register Project/Final Project/script.js
+++ JavaScript Algorithms and Data Structures/Build a Cash Register Project/Final Project/script.js
@@ -1,12 +1,32 @@
-let price = 1.87;
+// let price = 1.87;
+let price = 20;
 let cid = [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100],
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
 ];
+
+const cashElement = document.getElementById("cash");
+const changeDueElement = document.getElementById("change-due");
+
+const purchaseBtn = document.getElementById("purchase-btn");
+
+purchaseBtn.addEventListener("click", function () {
+  console.log("test")
+  if (cashElement.value < price) {
+  alert("Customer does not have enough money to purchase the item")
+} else if (cashElement.value = price) {
+  changeDueElement.innerText = "No change due - customer paid with exact cash"
+}
+})
+
+
+
+
+cashElement.innerText = ""
