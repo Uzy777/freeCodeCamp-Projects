9fff5b9f89df5b752f4e9507442467fe7a4f4f8c
Uzy <132500083+Uzy777@users.noreply.github.com>
2025-04-24 15:32:57 +0100

    Update In Progress Final Project Build a Cash Register

--- JavaScript Algorithms and Data Structures/Build a Cash Register Project/Final Project/script.js
+++ JavaScript Algorithms and Data Structures/Build a Cash Register Project/Final Project/script.js
@@ -21,9 +21,11 @@ purchaseBtn.addEventListener("click", function () {
   console.log("test")
   if (cashElement.value < price) {
   alert("Customer does not have enough money to purchase the item")
+} else if (cashElement.value > price) {
+  changeDueElement.innerText = "Status: OPEN QUARTER: $0.5" // This must be dynamic with literal ``
 } else if (cashElement.value = price) {
   changeDueElement.innerText = "No change due - customer paid with exact cash"
-}
+} 
 })
 
 
