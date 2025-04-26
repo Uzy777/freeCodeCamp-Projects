a9a6aa9bc20330329c4f473abccd43c11383fddd
Uzy777 <usmon.h@outlook.com>
2025-04-26 22:50:00 +0100

    Update In Progress Building a fCC Authors Page

--- JavaScript Algorithms and Data Structures/Build an RPG Creature Search App Project/Learn Fetch and Promises by Building an fCC Authors Page/script.js
+++ JavaScript Algorithms and Data Structures/Build an RPG Creature Search App Project/Learn Fetch and Promises by Building an fCC Authors Page/script.js
@@ -1,15 +1,15 @@
-const authorContainer = document.getElementById('author-container');
-const loadMoreBtn = document.getElementById('load-more-btn');
+const authorContainer = document.getElementById("author-container");
+const loadMoreBtn = document.getElementById("load-more-btn");
 
 let startingIndex = 0;
 let endingIndex = 8;
 let authorDataArr = [];
 
-fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
+fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
   .then((res) => res.json())
   .then((data) => {
     authorDataArr = data;
-    displayAuthors(authorDataArr.slice(startingIndex, endingIndex));  
+    displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
   })
   .catch((err) => {
     console.error(`There was an error: ${err}`);
@@ -20,7 +20,6 @@ const fetchMoreAuthors = () => {
   endingIndex += 8;
 
   displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
-
 };
 
 const displayAuthors = (authors) => {
@@ -36,4 +35,4 @@ const displayAuthors = (authors) => {
   });
 };
 
-loadMoreBtn.addEventListener('click', fetchMoreAuthors);
+loadMoreBtn.addEventListener("click", fetchMoreAuthors);
