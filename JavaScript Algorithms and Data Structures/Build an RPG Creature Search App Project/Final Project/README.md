26a6583d954632635cbe115e0644f57d947e3b28
Uzy777 <usmon.h@outlook.com>
2025-04-29 18:37:59 +0100

    Update Complete Final Project Build RPG Creature Search App

--- JavaScript Algorithms and Data Structures/Build an RPG Creature Search App Project/Final Project/README.md
+++ JavaScript Algorithms and Data Structures/Build an RPG Creature Search App Project/Final Project/README.md
@@ -2,3 +2,11 @@ This is one of the required projects to earn your certification.
 
 For this project, you will build an RPG creature search app.
 
+![alt text](image.png)
+
+![alt text](image-1.png)
+
+![alt text](image-2.png)
+
+![alt text](image-3.png)
+
--- JavaScript Algorithms and Data Structures/Build an RPG Creature Search App Project/Final Project/script.js
+++ JavaScript Algorithms and Data Structures/Build an RPG Creature Search App Project/Final Project/script.js
@@ -18,89 +18,65 @@ const creatureSpeed = document.getElementById("speed");
 
 const testP = document.getElementById("test");
 
+searchBtn.addEventListener("click", function () {
+  const nameOrId = searchInput.value;
 
-
-
-// fetch(`https://rpg-creature-api.freecodecamp.rocks/api/creatures`)
-
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error("Network response was not ok " + response.statusText);
-//     }
-//     return response.json();
-//   })
-
-//   .then(data => {
-//     console.log(data);
-//   })
-
-//   .catch(error => {
-//     console.error("There was a problem with the fetch operation:", error)
-//   })
-
-
-searchBtn.addEventListener("click", function(){
-
-  const nameOrId = searchInput.value
-
-  const url = `https://rpg-creature-api.freecodecamp.rocks/api/creature/${nameOrId}`
+  const url = `https://rpg-creature-api.freecodecamp.rocks/api/creature/${nameOrId}`;
 
   fetch(url)
-    .then(response => {
+    .then((response) => {
       if (!response.ok) {
         throw new Error("Creature not found");
       }
       return response.json();
     })
 
-    .then(data => {
+    .then((data) => {
       creatureName.innerText = JSON.stringify(data["name"]).slice(1, -1);
       creatureId.innerText = "#" + JSON.stringify(data["id"]);
       creatureWeight.innerText = "Weight: " + JSON.stringify(data["weight"]);
       creatureHeight.innerText = "Height: " + JSON.stringify(data["height"]);
-      
-      const typeNames = data.types.map(type => type.name).join(" ");
 
-      creatureType.innerText = typeNames.toUpperCase();
+      creatureType.innerHTML = "";
+
+      data.types.forEach((type) => {
+        const typeElement = document.createElement("p");
+        typeElement.innerText = type.name.toUpperCase();
+        creatureType.appendChild(typeElement);
+      });
 
       creatureSpecialName.innerText = JSON.stringify(data.special["name"]).slice(1, -1);
       creatureSpecialDescription.innerText = JSON.stringify(data.special["description"]).slice(1, -1);
 
-      const hpStat = data.stats.find(stat => stat.name === "hp")
+      const hpStat = data.stats.find((stat) => stat.name === "hp");
       creatureHp.innerText = hpStat.base_stat;
 
-      const attackStat = data.stats.find(stat => stat.name === "attack")
+      const attackStat = data.stats.find((stat) => stat.name === "attack");
       creatureAttack.innerText = attackStat.base_stat;
 
-      const defenseStat = data.stats.find(stat => stat.name === "defense")
+      const defenseStat = data.stats.find((stat) => stat.name === "defense");
       creatureDefense.innerText = defenseStat.base_stat;
 
-      const specialAttackStat = data.stats.find(stat => stat.name === "special-attack")
+      const specialAttackStat = data.stats.find((stat) => stat.name === "special-attack");
       creatureSpecialAttack.innerText = specialAttackStat.base_stat;
 
-      const specialDefenseStat = data.stats.find(stat => stat.name === "special-defense")
+      const specialDefenseStat = data.stats.find((stat) => stat.name === "special-defense");
       creatureSpecialDefense.innerText = specialDefenseStat.base_stat;
 
-      const speedStat = data.stats.find(stat => stat.name === "speed")
+      const speedStat = data.stats.find((stat) => stat.name === "speed");
       creatureSpeed.innerText = speedStat.base_stat;
-      
-      
 
       testP.innerText = JSON.stringify(data);
-      
+
       console.log(JSON.stringify(data));
     })
 
-    .catch(error => {
-      alert("Creature not found")
+    .catch((error) => {
+      alert("Creature not found");
       // console.error("There was a problem with the fetch operation:", error)
-    })
-
-
+    });
 
   // if (searchInput.value === "Red") {
   //   alert("Creature not found")
   // }
-}) 
-
-
+});
