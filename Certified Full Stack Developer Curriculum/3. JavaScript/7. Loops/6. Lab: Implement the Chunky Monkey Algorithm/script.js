73bf8dcd981b661efff212edf77567e537edbeaf
Uzy <132500083+Uzy777@users.noreply.github.com>
2025-09-22 12:14:06 +0100

    Update Complete - 7. Loops - 6. Lab: Implement the Chunky Monkey Algorithm

---
+++ Certified Full Stack Developer Curriculum/3. JavaScript/7. Loops/6. Lab: Implement the Chunky Monkey Algorithm/script.js
@@ -0,0 +1,18 @@
+function chunkArrayInGroups(arr, num) {
+
+    const result = [];
+
+    for (let i = 0; i < arr.length; i += num) {
+
+        result.push(arr.slice(i, i + num))
+
+    }
+    return result
+
+}
+
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2))
+
+
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3))
