fe846d9785aade69e1803ab4a50ba777cc8e09d7
Uzy <132500083+Uzy777@users.noreply.github.com>
2025-12-19 09:15:32 +0000

    Update Complete - 1. Variables and Strings - 5. Lab: Build a JavaScript Trivia Bot

---
+++ Certified Full Stack Developer Curriculum/2. JavaScript Certification/1. JavaScript/1. Variables and Strings/5. Lab: Build a JavaScript Trivia Bot/script.js
@@ -0,0 +1,18 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Timmy Rodger";
+let botLocation = "United Kingdom";
+let favoriteLanguage = "JavaScript";
+let codingFact = favoriteLanguage + " is a versatile language used for web development!";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+console.log(codingFact);
+
+codingFact = "Just kidding! " + favoriteLanguage + " is the best language for beginners!";
+console.log(codingFact);
+
+codingFact = "Actually, " + favoriteLanguage + " is great for both front-end and back-end development!";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
