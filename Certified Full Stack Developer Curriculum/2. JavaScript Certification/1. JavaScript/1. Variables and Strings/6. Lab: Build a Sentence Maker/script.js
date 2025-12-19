e4510ef23c8f4f35aa35debe3cb21c4e3bacc3be
Uzy <132500083+Uzy777@users.noreply.github.com>
2025-12-19 09:16:25 +0000

    Update Complete - 1. Variables and Strings - 6. Lab: Build a Sentence Maker

---
+++ Certified Full Stack Developer Curriculum/2. JavaScript Certification/1. JavaScript/1. Variables and Strings/6. Lab: Build a Sentence Maker/script.js
@@ -0,0 +1,22 @@
+let adjective = "colorful";
+let noun = "butterfly";
+let verb = "flutter";
+let place = "garden";
+let adjective2 = "beautiful";
+let noun2 = "sunset";
+
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "mysterious";
+noun = "forest";
+verb = "explore";
+place = "mountain";
+adjective2 = "majestic";
+noun2 = "waterfall";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
