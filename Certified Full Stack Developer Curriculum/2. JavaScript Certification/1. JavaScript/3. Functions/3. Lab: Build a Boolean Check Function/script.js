354dbd44dcea3094dd77d460f89b2ff4263d693a
Uzy <132500083+Uzy777@users.noreply.github.com>
2026-01-06 09:57:19 +0000

    Update Complete - 3. Functions - 3. Lab: Build a Boolean Check Function

---
+++ Certified Full Stack Developer Curriculum/2. JavaScript Certification/1. JavaScript/3. Functions/3. Lab: Build a Boolean Check Function/script.js
@@ -0,0 +1,16 @@
+const booWho = (x) => {
+    return typeof x === "boolean";
+}
+
+
+
+
+
+// const booWho = (x) => {
+//   if (x === true) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
