60472192b4edee7ddbdd334359d8b08f2440a649
Uzy <132500083+Uzy777@users.noreply.github.com>
2026-06-04 13:51:01 +0100

    Complete - 1. React Fundamentals - 3. Lab: Build a Reusuable Footer

---
+++ Certified Full Stack Developer Curriculum/3. Front-End Development Libraries/1. React Fundamentals/3. Lab: Build a Reusuable Footer/index.jsx
@@ -0,0 +1,32 @@
+export const Footer = () => {
+    return (
+        <footer>
+            <div className="footer-columns">
+                <ul>
+                    <li>abc</li>
+                    <li>abc</li>
+                </ul>
+
+                <ul>
+                    <li>abc</li>
+                    <li>abc</li>
+                </ul>
+
+                <ul>
+                    <li>abc</li>
+                    <li>abc</li>
+                </ul>
+            </div>
+
+            <p>© 2026 Uzy Dashboard. All rights reserved.</p>
+
+            <div id="icons">
+                <a href="#">🍱</a>
+                <a href="#">😤</a>
+                <a href="#">🦣</a>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
